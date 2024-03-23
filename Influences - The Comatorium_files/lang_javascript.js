5baf5b4c84cb356c599c735727195e40e6298943
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-03-23 16:23:57 -0400

    commit

---
+++ Influences - The Comatorium_files/lang_javascript.js
@@ -0,0 +1,100 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/**
+* JS Language File: English
+*/
+
+var message_pop_up_lang =
+{
+	// DEFAULT
+	'error'			   : 'An Error Occurred',
+	'no_permission'	   : 'You do not have permission for that action',
+	'cannot_cont'	   : 'Cannot continue with this request',
+	'settings_updated' : 'Settings Updated',
+	
+	// Custom errors:
+	'pp_date_error'     : 'Date must be a legal valid date',
+	'pp_comment_error'  : 'You cannot post empty comments',
+	'pp_friend_exist'   : 'That member does not appear to exist',
+	'pp_friend_already' : 'You already have that member as your friend',
+	'pp_friend_removed' : 'Friend(s) Removed',
+	'pp_icq_error'      : 'Your ICQ identity must be numeric',
+	
+	// Friend added:
+	'pp_friend_added'     : 'Your friend(s) have been added to your list!',
+	'pp_friend_added_mod' : 'Your friend must approve your request',
+	'pp_friend_approved'  : 'Friend request approved',
+	
+	// Comments update
+	'pp_comments_updated'  : 'Comments updated',
+	'pp_comment_added_mod' : "Comment must be approved before it's shown",
+	
+	// POLL MODULE
+	'already_voted' : 'You have already voted',
+	
+	// ARTICLES MODULE
+	'rating_updated' : 'Your rating has been updated',
+	'rating_added'   : 'Your rating has been added',
+	'rating_already' : 'You have rated this article already'
+};
+
+/**
+* Global stuff
+*/
+
+var ipb_global_lang =
+{
+	// 2.3.2
+	'action_cancelled'	  	  : 'OK, action cancelled!',
+	// ERROR
+	'post_upload_not_empty'   : 'You have an item to be uploaded. Click the UPLOAD button to upload it or click OK to continue without uploading',
+	// EDITORS
+	'editor_enter_url'		  : 'Please enter the full URL',
+	'editor_enter_title'      : 'Please enter the title for this item',
+	'editor_enter_email'      : 'Please enter the email address',
+	'editor_enter_image'      : 'Please enter the URL for this image',
+	'editor_enter_list'       : 'Enter a list item',
+	// GENERAL
+	'general_error'           : 'Error',
+	'general_ok'              : 'OK',
+	'image_resized'           : 'Reduced: <%1>% of original size [ <%2> x <%3> ] - Click to view full image',
+	'image_attach_percent'    : 'Reduced <%1>%',
+	'image_attach_no_percent' : 'Image Reduced',
+	'image_attach_dims'       : '<%1> x <%2>',
+	'image_attach_size'       : ' (<%1>)',
+	'click_to_view'           : 'Click to view full image',
+	'view_full_size'          : 'View full size image',
+	'visit_my_website'		  : 'Visit My Website'
+};
+
+}
+/*
+     FILE ARCHIVED ON 22:47:34 Sep 30, 2011 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 04:59:37 Nov 21, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1029.946
+  exclusion.robots: 0.069
+  exclusion.robots.policy: 0.06
+  cdx.remote: 0.053
+  esindex: 0.009
+  LoadShardBlock: 1000.568 (3)
+  PetaboxLoader3.datanode: 1001.101 (4)
+  load_resource: 60.443
+  PetaboxLoader3.resolve: 29.353
+*/
